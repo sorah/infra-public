0e7043a549b1b62d8e77781dc2bd55f1a18f34a7
Sorah Fukumori <her@sorah.jp>
2019-11-09 03:59:11 +0900

    fluentd cookbook

---
+++ itamae/cookbooks/arch/fluentd/default.rb
@@ -0,0 +1,59 @@
+node.reverse_merge!(
+  fluentd: {
+    config_path: '/etc/fluent/fluent.conf',
+  },
+)
+if node.dig(:prometheus, :exporter_proxy)
+  node[:prometheus][:exporter_proxy][:exporters][:fluentd] = {path: '/fluentd/metrics', url: 'http://localhost:24231/metrics'}
+end
+
+gem_package 'fluentd'
+gem_package 'fluent-plugin-prometheus'
+
+user 'fluentd' do
+  system_user true
+end
+
+directory '/etc/fluent' do
+  owner 'root'
+  group 'root'
+  mode  '0755'
+end
+
+directory '/var/lib/fluentd' do
+  owner 'fluentd'
+  group 'fluentd'
+  mode  '0755'
+end
+
+directory '/var/lib/fluentd/buffer' do
+  owner 'fluentd'
+  group 'fluentd'
+  mode  '0755'
+end
+
+directory '/usr/share/fluentd' do
+  owner 'root'
+  group 'root'
+  mode  '0755'
+end
+
+directory '/usr/share/fluentd/plugins' do
+  owner 'root'
+  group 'root'
+  mode  '0755'
+end
+
+directory '/var/log/fluentd' do
+  owner 'fluentd'
+  group 'fluentd'
+  mode  '0755'
+end
+
+
+template '/etc/systemd/system/fluentd.service' do
+  owner 'root'
+  group 'root'
+  mode  '0644'
+  notifies :run, 'execute[systemctl daemon-reload]'
+end
