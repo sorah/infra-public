e0b958c19a46798be21034f6cfbbab441620da7c
Sorah Fukumori <her@sorah.jp>
2020-01-19 17:29:09 +0900

    vault-cert: Enhancement for CA rotation
    - now generates trusted CA File to allow using previous/next CA
    - always renew certificate when :pki is changed

--- itamae/cookbooks/common/vault-cert/default.rb
+++ itamae/cookbooks/common/vault-cert/default.rb
@@ -4,7 +4,7 @@ node.reverse_merge!(
     token_file: '/root/.vault-token',
     env_file: nil,
     certs: {
-      # key: { pki:, role:, ca_file:, cert_file:, fullchain_file: key_file:, owner:, group:, mode:, sans:, cn:, ip:, units_to_reload:, units_to_restart:, threshold_days:, }
+      # key: { pki:, role:, ca_file:, cert_file:, fullchain_file: key_file:, trust_ca_file: trust_pkis:, owner:, group:, mode:, sans:, cn:, ip:, units_to_reload:, units_to_restart:, threshold_days:, }
     },
   },
 )
@@ -16,12 +16,14 @@ file '/etc/nkmi-vault-cert.json' do
   owner 'root'
   group 'root'
   mode  '0600'
+  notifies :run, 'execute[systemctl start --no-block nkmi-vault-cert.service]'
 end
 
 remote_file '/usr/bin/nkmi-vault-cert' do
   owner 'root'
   group 'root'
   mode  '0755'
+  notifies :run, 'execute[systemctl start --no-block nkmi-vault-cert.service]'
 end
 
 template '/etc/systemd/system/nkmi-vault-cert.service' do
@@ -36,6 +38,10 @@ remote_file '/etc/systemd/system/nkmi-vault-cert.timer' do
   group 'root'
   mode  '0644'
   notifies :run, 'execute[systemctl daemon-reload]'
+end
+
+execute 'systemctl start --no-block nkmi-vault-cert.service' do
+  action :nothing
 end
 
 service 'nkmi-vault-cert.timer' do
