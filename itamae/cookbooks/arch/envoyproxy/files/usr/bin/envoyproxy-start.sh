5603584eb60b1b46e1c325f8d2cd6cad0f44c6e6
Sorah Fukumori <her@sorah.jp>
2020-01-24 05:01:04 +0900

    envoyproxy: --drain-time-s 60

--- itamae/cookbooks/arch/envoyproxy/files/usr/bin/envoyproxy-start.sh
+++ itamae/cookbooks/arch/envoyproxy/files/usr/bin/envoyproxy-start.sh
@@ -2,6 +2,7 @@
 exec /usr/bin/envoy \
   --restart-epoch ${RESTART_EPOCH} \
   --config-path /etc/envoyproxy/${ENVOY_CONFIG_NAME}.json \
+  --drain-time-s ${ENVOY_DRAIN_TIME:-60} \
   --service-cluster '<%= node.dig(:envoy,:service_cluster) || "default" %>' \
   --service-node '<%= node.dig(:envoy,:service_node) || node[:desired_hostname] || node[:hostname] %>' \
   --service-zone '<%= node.dig(:envoy,:service_zone) || "default" %>' \
