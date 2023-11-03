5a624ab17b63af1471151c5055d71cfaaa46e11d
Sorah Fukumori <her@sorah.jp>
2023-11-04 05:39:10 +0900

    arch/ruby

---
+++ itamae/cookbooks/arch/ruby/default.rb
@@ -0,0 +1,15 @@
+package 'ruby' do
+end
+package 'ruby-irb' do
+end
+package 'ruby-rdoc' do
+end
+package 'ruby-rake' do
+end
+package 'ruby-bundler' do
+end
+package 'ruby-bundler1' do
+end
+
+
+include_cookbook 'gemrc'
